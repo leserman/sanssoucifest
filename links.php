500b99fa07fbbe93bb3c942b1ea190e14f15eb11
leserman <david@leserman.com>
2015-01-05 11:23:18 -0700

    replace stale version

--- links.php
+++ links.php
@@ -45,37 +45,70 @@
 	  	<td width="530" align="center" valign="top" class="bodyTextGrayLight"><!-- InstanceBeginEditable name="ProgramRegion" -->
 	  	  <table width="100%" border="0" cellpadding="0" cellspacing="0" class="programTablePageBackground">
           <tr>
-            <td colspan="3" align="left" valign="middle" class="programPageTitleText" style="padding-top:12px;">Links<br>
+            <td colspan="3" align="left" valign="middle" class="programPageTitleText" style="padding-top:12px;">Links&nbsp;&nbsp;<span class="bodyTextOnBlack">(<a href="#supporters">Supporters</a>, <a href="#artists">Artists</a>, <a href="#venues">Venues</a>, <a href="#resources">Resources</a>)</span><br>
       <img src="images/dotClear.gif" alt="" width="1" height="6" hspace="0" vspace="0" border="0" align="middle"></td>
           </tr>
           <tr>
             <td colspan="3" align="left" valign="top">
-              <div class="linkListTitle">Resources</div>
-						  <div class="linkListText"><a href="http://www.dancefilmsassn.org/">Dance Films Association, DFA</a>, aims to impart the spirit of collaboration that must be inherent in dance-cinema works and to encourage a keen awareness of the magical ways to interpret time, space, and energy. Of interest is their <a href="http://www.dancefilms.org/resources/dance-and-media-timeline/">Dance and Media Timeline</a>.
+            
+              <!-- Supporters -->
+              <div id="supporters" class="linkListTitle">Supporters</div>
+              <div class="linkListText">
+                <a href="http://www.bouldercountyarts.org/"><img src="images/logos/BCAA-GRN-logo200.gif" 
+                           alt="Boulder Country Arts Alliance - BCAA" width="200" height="73" hspace="2" vspace="0" border="0" 
+                           align="middle" title="Boulder Country Arts Alliance - BCAA"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="http://www.artsresource.org/"><img src="images/logos/BAC-Low_Horizontal2.gif" 
+                          alt="Boulder Arts Commission" width="200" height="45" hspace="2" vspace="0" border="0" align="middle"></a>
               </div>
-<!--						  <div class="linkListText">The <b>Who are we?</b> tab at <a href="http://www.media-dance.com/">Media and Dance (MAD) network</a> is an index of dance film festivals around the world.</div> -->
+					    <div class="linkListText"><a href="http://theatredance.colorado.edu/dance/">Dance Division</a> of the <a href="http://www.colorado.edu/TheatreDance/">Theatre &amp; Dance Department</a>, at the <a href="http://www.colorado.edu/">University of Colorado at Boulder</a>
+					    </div>
+					    <div class="linkListText">Atlas <a href="http://www.colorado.edu/atlas/newatlas/amp/">Center for Media, Arts and Performance</a> at the University of Colorado at Boulder
+					    </div>
+					    <div class="linkListText"><a href="http://www.colorado.edu/FilmStudies/">Film Studies Program</a> at the University of Colorado at Boulder
+					    </div>
+					    <div class="linkListText"><a href="http://www.artsresource.org/dance-bridge/">Dance Bridge</a> and <a href="http://bplnow.boulderlibrary.org/event/movies">Boulder Public Library Cinema Program</a>
+					    </div>
+
+					    
+
+					    <!-- Exhibited Artists & Works -->
+              <div id="artists" class="linkListTitle">Exhibited Artists &amp; Works</div>
+              <div class="linkListText">Links to exhibited artists and works can be found on the corresponding Festival and Tour Archive pages in our navigation bar to the left.</div>
+					    
+              <!-- Venues -->
+              <div id="venues" class="linkListTitle">Venues</div>
+						  <div class="linkListText"><span class="yearsAtVenue">2010&ndash;2013: </span>The primary venue for Sans Souci for the years 2010 through 2013 is the
+						    Atlas <a href="http://www.colorado.edu/atlas/newatlas/amp/">Center for Media, Arts and Performance</a> at the University of Colorado at Boulder.
+						  </div>
+						  <div class="linkListText"><span class="yearsAtVenue">2009 &amp; 2013: </span><a href="http://thedairy.org//">The Dairy Center for the Arts</a> is a cultural arts complex in Boulder, Colorado, founded in 1992 and housed in the historic Watts-Hardy Dairy building. It features dramatic theater, live music, dance performances, and visual arts.
+                The Dairy was the venue for our 2009 Festival. Additional screenings for the 2013 Tenth Anniversary Festival Event will be presented in the Dairy's
+                <a href="http://www.thedairy.org/boedecker-theater2/">Boedecker Theater</a>.
+              </div>
+						  <div class="linkListText"><span class="yearsAtVenue">2004&ndash;2008: </span><a href="http://www.bmoca.org/">Boulder Museum of Contemporary Art, BMoCA</a>, originally called the Boulder Arts Center, was founded in 1972 to showcase and promote the visual arts in Boulder, Colorado. The museum is committed to providing local 
+                and regional artists, as well as recognized artists, 	with a significant venue. It is housed in an historic warehouse that now includes three galleries 
+                and a 100-seat black box theater. BMoCA hosted Sans Souci in the years 2004 through 2008.
+              </div>
+  					  <div class="linkListText"><span class="yearsAtVenue">2008 &amp; 2010: </span>Founded in 1989, the mission of <a href="http://www.highwaysperformance.org/">Highways</a> is to
+    					  cultivate art that expands the frontiers of aesthetic traditions.
+    				  </div>
+    				  <div class="linkListText"><span class="yearsAtVenue">2013: </span>Here is a <a href="http://www.youtube.com/watch?v=nn_uPCZiXoo">video of Ana Baer describing the March 2013 event</a> sponsored by the <a href="http://www.theatreanddance.txstate.edu/dance.html">Dance Division</a> at <a href="http://www.txstate.edu/">Texas State University</a>, San Marcos.
+    				  </div>
+					    <div class="linkListText"><span class="yearsAtVenue">2011, 2012 &amp; 2013: </span><a href="http://atticrep.org/site/sans-souci/">AtticRep</a>, <a href="http://web.trinity.edu/">Trinity University</a>, San Antonio TX, USA.
+					    </div>
+					  
+              <!-- Resources -->
+              <div id="resources" class="linkListTitle">Resources</div>
+						  <div class="linkListText"><a href="http://www.dancefilms.org/">Dance Films Association, DFA</a>, is dedicated to furthering the art of dance film. Of interest is their <a href="http://www.dancefilms.org/resources/other-dance-film-festivals/">listing of dance film festivals around the world</a> and their <a href="http://www.dancefilms.org/resources/dance-and-media-timeline/">Dance and Media Timeline</a>.
+              </div>
+              <!--						  <div class="linkListText">The <a href="http://www.media-dance.com/">Media Dance</a> page at the <i>Cinedans Dance on Screen Festival</i> in Amsterdam is an index of dance film festivals around the world.</div> -->
 	  				  <div class="linkListText"><a href="https://www.jiscmail.ac.uk/cgi-bin/webadmin?A0=MEDIA-ARTS-AND-DANCE">MEDIA-ARTS-AND-DANCE </a> is an email list and archive used by its members to announce and discuss theory, research, critical discourse, publications, conferences, networking and curatorial approaches 
                 in the field of screen based dance, including single screen cinema or television works, installation, and net based work. 
                 <!-- The list is maintained by <a href="http://www.dance-tech.net/profiles/blog/list?user=ue29v6z6ebxf">Simon Fildes</a>. -->
               </div>
-						  <div class="linkListText">Katrina McPherson's <a href="http://www.makingvideodance.com/">Making Video Dance</a>, describes her book, a step by step guide to creating dance for the screen, and includes her <a href="http://www.makingvideodance.com/Resources.html">videodance twitter feed</a>.</div>
+<!--						  <div class="linkListText">Katrina McPherson's <a href="http://www.makingvideodance.com/">Making Video Dance</a>, describes her book, a step by step guide to creating dance for the screen.</div> Removed 4/5/2014 becasue the site appears to have petered out. -->
 	  				  <div class="linkListText"><a href="http://www.dance-tech.net/">DANCE-TECH.<i>NET,</i> Interdisciplinary explorations on the performance of motion</a>,
                 is an international community of artists, scientists, theorists, and organizations exploring the intersection of performance, media, and culture.</div>
-              <div class="linkListTitle">Sans Souci Venues</div>
-						  <div class="linkListText"><a href="http://www.bmoca.org/">Boulder Museum of Contemporary Art, BMoCA</a>, originally called the Boulder Arts Center, was 
-                founded in 1972 to showcase and promote the visual arts in Boulder, Colorado. The museum is committed to providing local 
-                and regional artists, as well as recognized artists, 	with a significant venue. It is housed in an historic warehouse that now includes three galleries 
-                and a 100-seat black box theater. BMoCA hosted Sans Souci in the years 2004 through 2008.</div>
-  					  <div class="linkListText">Founded in 1989, the mission of <a href="http://www.highwaysperformance.org/">Highways</a> is to
-    					  cultivate art that expands the frontiers of aesthetic traditions.</div>
-						  <div class="linkListText"><a href="http://thedairy.org//">The Dairy Center for the Arts</a> is a cultural arts complex in Boulder, Colorado, founded in 1992
-                and housed in the historic Watts-Hardy Dairy building. It features dramatic theater, live music, dance performances, and visual arts.
-                The Dairy will be the venue for our 2009 Festival.</div>
-					  <div class="linkListText"><a href="http://www.colorado.edu/TheatreDance/">Theatre &amp; Dance Department, University of Colorado at Boulder</a></div>
-					  <div class="linkListText">Here is a <a href="http://www.youtube.com/watch?v=nn_uPCZiXoo">video of Ana Baer describing the March 2013 event</a> sponsored by the <a href="http://www.theatreanddance.txstate.edu/dance.html">Dance Division</a> at <a href="http://www.txstate.edu/">Texas State University</a>, San Marcos.</div>
-					  <div class="linkListText"><a href="http://atticrep.org/site/sans-souci/">AtticRep</a>, <a href="http://web.trinity.edu/">Trinity University</a>, San Antonio TX, USA.</div>
-            <div class="linkListTitle">Exhibited Artists &amp; Works</div>
-            <div class="linkListText">Links to exhibited artists and works can be found on the corresponding Festival and Tour Archive pages in our navigation bar to the left.</div>
+                
 				  </td>
           </tr>
           <!--TEMPLATE -->
