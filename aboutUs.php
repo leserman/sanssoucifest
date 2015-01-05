a752b85b4bc05221754ba9d12b9f2ade15ca3101
leserman <david@leserman.com>
2015-01-05 12:27:44 -0700

    replace stale version

--- aboutUs.php
+++ aboutUs.php
@@ -1,100 +1,95 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html><!-- InstanceBegin template="/Templates/program.dwt" codeOutsideHTMLIsLocked="false" -->
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
-  <meta http-equiv="Pragma" content="no-cache">
-  <meta http-equiv="Expires" content="-1">
-  <meta name="description" content="A niche film festival specializing in dance cinema, incorporating live performance, and including museum installations.">
-  <meta name="keywords" content="dance film festival, dance video festival, video dance festival, dance cinema festival, live performance, dance performance, live dance performance, dance festival, video dance, dance video, dance film, dance cinema, dance, film, video, cinema, festival, art, arts, artists, projection, projected, tour, touring"><!-- InstanceBeginEditable name="doctitle" -->
-<title>Sans Souci Festival of Dance Cinema - About Us</title>
-<!-- InstanceEndEditable --><!-- <base href="http://www.sansoucifest.org/"> -->
-  <link rel="stylesheet" href="sanssouci.css" type="text/css">
-  <link rel="stylesheet" href="sanssouciBlackBackground.css" type="text/css">
-</head>
-<body bgcolor="#FFFFFF" text="#000000" link="#0033FF" vlink="#0033FF" alink="#990000">
-  <!-- Set bgcolor="#FFFFFF" in the following 100% table to make the edges appear white. -->
-  <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#000000">
-    <tr>
-      <td align="left" valign="top">
-        <table width="745" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#000000">
-          <tr>
-            <td colspan="3" align="left" valign="top"><a href="index.php"><img src="images/titleBarGrayLight.gif" alt="SansSouciFest.org" width="600" height="63" hspace="0" vspace="8" border="0" align="top"></a></td>
-            <td width="10" align="center" valign="top">&nbsp;</td>
-          </tr>
-          <tr>
-            <td width="10" align="center" valign="top">&nbsp;</td>
-            <td width="125" align="center" valign="top">
-            <?php
-              $filePathArray = explode('/', __FILE__);
-              $loopIndex = 0;
-              foreach ($filePathArray as $element) { 
-                $loopIndex++;
-                if ($element == 'sanssoucifest.org') { break; } 
-              }
-              $codeBase = "";
-              for ($i = ($loopIndex+1); $i <= (sizeof($filePathArray)-1); $i++) { $codeBase .= '../'; }
-              include_once $codeBase . "bin/utilities/autoloadClasses.php";
-              SSFWebPageAssets::displayNavBar();
-            ?></td>
-            <td width="600" align="center" valign="top">
-              <table width="100%" align="center" cellpadding="0" cellspacing="0" bgcolor="#000000">
-                <tr>
-                  
-	  <td width="25" align="left" valign="top" class="sprocketHoles">&nbsp;</td>
-    <td width="10" align="left" valign="top" class="programTablePageBackground">&nbsp;</td>
-	  	<td width="530" align="center" valign="top" class="bodyTextGrayLight"><!-- InstanceBeginEditable name="ProgramRegion" -->
-			
-   <table width="100%"  border="0" cellpadding="0" cellspacing="0" bgcolor="#333333">
-		  <tr>
-        <td align="left" valign="top" class="programPageTitleText" style="padding-top:12px;">About Us</td>
-		  </tr>
-			<tr>
-			  <td align="left" valign="top" class="bodyTextLeadedOnBlack" style="padding:0 20px 0 20px;"><br>
-			    Sans Souci (meaning &quot;without concern&quot;) was conceived one fine spring day in 2003 when <a href="http://michelleellsworth.com">Michelle
-			      Ellsworth</a> and Brandi Mathis sat  on the 
-          porch of a 1967 Marlette Mobile home in the Sans Souci Trailer Park in  Boulder Colorado musing about the pleasures
-			      of viewing and creating dances for the screen. Quickly <a href="http://www.bmoca.org">BMoCA</a> and The University of Colorado as well as artists <a href="http://avantmedia.org/artists/principal.html">Ana
-          Baer</a> and Hamel Bloom added their support to transform mere 
-          musing into a Festival of Dance Cinema.  What was first envisioned in '03 as an informal gathering of local
-          dance video artists screening their works on a white wall in a trailer is now an international festival housed
-          at BMoCA with submissions from all over the world.<br>
-          <br>
-          Sans Souci Festival of Dance Cinema started in Boulder Colorado in
-                        2003 with the support of the <a href="http://www.bmoca.org/">Boulder Museum of Contemporary Art</a> (BMoCA)
-                        and the <a href="http://www.colorado.edu/TheatreDance/">University of Colorado at Boulder</a>. With
-                        an expansive definition of <i>dance</i> and an appreciation for highly experimental and interdisciplinary
-                        forms, this unique festival exposes diverse audiences to a variety of film, video, and performance
-                        possibilities.</div>
-												<br><br>
-          Directors<br>
-					&nbsp;&nbsp;&nbsp;Ana Baer, Artistic Co-Director<br>
-					&nbsp;&nbsp;&nbsp;Michelle Ellsworth, Artistic Co-Director<br>
-					&nbsp;&nbsp;&nbsp;Hamel Bloom, Executive Director<br>
-					<img src="images/dotClear.gif" alt="" width="1" height="24" hspace="0" vspace="0" border="0" align="middle"></td>
-			</tr>
-		</table>			
-			
-<!-- InstanceEndEditable --></td>
-    <td width="10" align="left" valign="top" class="programTablePageBackground">&nbsp;</td>
-	  <td width="25" align="left" valign="top" class="sprocketHoles">&nbsp;</td>
+<!DOCTYPE html>
+<?php 
+  include_once './bin/classes/SSFCodeBase.php'; 
+  include_once SSFCodeBase::autoloadClasses(__FILE__);
+  SSFWebPageParts::initializePage(__FILE__); 
+
+  /* UPDATE THESE ITEMS */
+    SSFWebPageParts::setContentColumnCount(1);
+    SSFWebPageParts::disallowRobotIndexing(); // default is to allow robot indexing
+    SSFWebPageParts::setPageHeaderTitleText('About Us');
+    SSFWebPageParts::setPageHeaderTitleTextAlignment('topLeft'); // default is topLeft
     
-                </tr>
-              </table>
-            </td>
-            <td width="10" align="center" valign="top">&nbsp;</td>
-          </tr>
-          <tr align="center" valign="top">
-            <td colspan="2">&nbsp;</td>
-            <td align="center" valign="bottom" class="smallBodyTextLeadedGrayLight"><br>
-            <?php SSFWebPageAssets::displayCopyrightLine();?></td>
-            <td width="10">&nbsp;</td>
-          </tr>
-          <tr align="center" valign="top">
-            <td colspan="4">&nbsp;</td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-  </table>
-</body>
-<!-- InstanceEnd --></html>
+  echo SSFWebPageParts::beginHTML();
+  echo SSFWebPageParts::htmlHeadContent(); 
+?>
+    <style type="text/css"> /* CSS inline style definitions intended for this page only go here. */
+      p { width:430px; } 
+      p:first-of-type { margin-top:4px; }
+      div.content { margin:10px 0 36px 0;}
+/*      div { color:#FFFF99;margin:20px 0 -4px 0;font-size:16px;font-weight:bold; } */
+      li { margin-bottom: 3px; margin-left: 20px; padding-left: 6px; }
+      ul { margin-left: 0; }
+    </style>
+<?php 
+  echo SSFWebPageParts::cssMediaQueries(); 
+  echo SSFWebPageParts::endHead();
+  echo SSFWebPageParts::beginPageBody();
+  echo SSFWebPageParts::beginContentHeader();
+?>
+<!-- 26 spaces         -->
+                          <div class="content">
+                            <p style="margin-left:20px;padding-left:0;"><a href="#beginnings">Beginnings</a>&nbsp;&nbsp;<a href="#vision">Vision</a>&nbsp;&nbsp;<a href="#mission">Mission</a>&nbsp;&nbsp;<a href="#history">History</a>&nbsp;&nbsp;<a href="#directors">Directors</a>&nbsp;&nbsp;<a href="#founders">Founders</a></p>
+                            <p style="margin:16px 12px 0 20px;">With an expansive definition of <i>dance</i> and an appreciation for highly experimental and interdisciplinary forms, this unique festival exposes diverse audiences to a variety of film, video, and performance possibilities.</p>
+                            <div class="sectionTitle" id="beginnings">Beginnings
+                              <p>Sans Souci (meaning &quot;without concern&quot;) was conceived one fine spring day in 2003 when Michelle Ellsworth and Brandi Mathis sat  on the porch of a 1967 Marlette Mobile home in the Sans Souci Trailer Park in  Boulder Colorado musing about the pleasures of viewing and creating dances for the screen. Quickly Boulder Museum of Contemporary Art (BMoCA) and the <img src="images/daisycrunchgraphic.jpg" alt="Our daisy-eating icon" style="width:75px;height:74px;float:right;margin:13px;">University of Colorado at Boulder <a href="http://www.colorado.edu/TheatreDance/">Department of Theatre and Dance</a>, as well as artists Ana Baer and Hamel Bloom, added their support to transform mere musing into an Festival of Dance Cinema.</p>
+                              <p>What was first envisioned as an informal gathering of local dance video artists screening their works on a white wall in a trailer is now an international festival with submissions from all over the world.</p>
+                            </div>
+                            <div class="sectionTitle" id="vision">Vision
+                              <p>We envision a world where many people are aware of dance cinema and many of them appreciate and enjoy the form. And a world where artists everywhere can get paid for their work, especially the dancers.</p>
+                            </div>
+                            <div class="sectionTitle" id="mission">Mission
+                              <p>Our mission is two-fold.</p>
+                              <ol style="margin-top:6px;">
+                                <li class="bodyTextLeadedOnBlack">Attract, educate, and entertain an audience for this form.</li>
+                                <li class="bodyTextLeadedOnBlack">Support the artists creating works in this form by providing audiences for their work.</li>
+                              </ol>
+                              <p>We seek to fulfill our mission in these ways:</p>
+                              <ul style="margin-bottom:0;">                                
+                                <li class="bodyTextLeadedOnBlack">Support new work that integrates dance with cinematic elements, both experimental and traditional.</li>
+                                <li class="bodyTextLeadedOnBlack">Encourage an expansive definition of dance and encourage an appreciation for highly experimental and interdisciplinary forms, including mixed-media works that incorporate live performance.</li>
+                                <li class="bodyTextLeadedOnBlack">Strive to slowly grow the festival each year with higher quality films and a larger, more satisfied audience.</li>
+                                <li class="bodyTextLeadedOnBlack">Provide a forum for the evolving conversation between dance and cinema - celebrating both and their potential to cross-pollinate.</li>
+                                <li class="bodyTextLeadedOnBlack">Show work in a professional and stress-free environment.</li>
+                                <li class="bodyTextLeadedOnBlack">Bring the work of international artists to Boulder, Colorado.</li>
+                                <li class="bodyTextLeadedOnBlack">Take the work of local artists to an international audience via the Sans Souci Tour.</li>
+                                <li class="bodyTextLeadedOnBlack">Develop an appreciation and appetite in our audiences for dance-for-camera.</li>
+                                <li class="bodyTextLeadedOnBlack">Provide support and feedback to our applicants.</li>
+                                <li class="bodyTextLeadedOnBlack">Snacks &ndash; always free snacks at all of our screenings.</li>
+                              </ul>
+                            </div>
+                            <div class="sectionTitle" id="history">History in a Nutshell
+                              <p>With ten years of annual festivals behind us, Sans Souci has exhibited 317 works submitted by about 200 artists from countries all over the world, including: Armenia, Australia, Belgium, Canada, Denmark, Finland, France, Germany, Hungary, Mexico, Netherlands, Norway, Portugal, Scotland, South Korea, Spain, Sweden, Switzerland, Turkey, UK, and USA. Our international tours have brought works to Mexico (2004, 2006 &amp; 2012), Germany (2007 &amp; 2009), Spain (2010), and Trinidad &amp; Tobago (2004).</p>
+                              <p>Highlights &amp; Milestones:
+                              <ul>
+                                <li class="bodyTextLeadedOnBlack">2004 - 1st annual festival at BMoCA. Tour in Mexico.</li>
+                                <li class="bodyTextLeadedOnBlack">2005 - 2nd annual festival at BMoCA.</li>
+                                <li class="bodyTextLeadedOnBlack">2006 - Fleshed out web site. Tour in Mexico.</li>
+                                <li class="bodyTextLeadedOnBlack">2007 - Added online entry & automated assistance for curation. Tour in Germany.</li>
+                                <li class="bodyTextLeadedOnBlack">2008 - Tour events in Los Angeles and Texas.</li>
+                                <li class="bodyTextLeadedOnBlack">2009 - Brought multimedia performance-artist/dancer from Finland.</li>
+                                <li class="bodyTextLeadedOnBlack">2010 - Presented a Scholarly Panel of dance-film artists. Tour in Spain.</li>
+                                <li class="bodyTextLeadedOnBlack">2011 - Held multiple screenings at two distinct venues.</li>
+                                <li class="bodyTextLeadedOnBlack">2012 - Survived without financial support.</li>
+                                <li class="bodyTextLeadedOnBlack">2013 - Screened 5 distinct programs at 3 venues.</li>
+                                <li class="bodyTextLeadedOnBlack">2014 - Tour at the Roxy in Missoula MT.
+                              </ul>
+                              <img src="images/chartOfWorksSubmittedAndAccepted433x305.jpg" alt="Chart showing number of works submitted and accepted by year" style="border:0;width:433px;height:305px;margin-left:40px;">
+                            </div>
+                            <div class="sectionTitle" id="directors">Directors
+                              <p style="margin-bottom:4px;margin-top:4px;">&nbsp;&nbsp;&nbsp;<a href="http://anabaer.com/">Ana Baer</a>, Artistic Co-Director<br>
+                              &nbsp;&nbsp;&nbsp;<a href="www.tararynders.com">Tara Rynders</a>, Artistic Co-Director<br>
+                              &nbsp;&nbsp;&nbsp;Hamel Bloom, Executive Director</p>
+                            </div>
+                            <div class="sectionTitle" id="founders">Founders
+                              <p style="margin-bottom:4px;margin-top:4px;">&nbsp;&nbsp;&nbsp;<a href="http://michelleellsworth.com">Michelle Ellsworth</a>, performer extraordinaire, Professor of Dance</p>
+                              <p style="margin-bottom:4px;margin-top:4px;">&nbsp;&nbsp;&nbsp;Brandi Mathis, curator extraordinaire</p>
+                            </div>
+                          </div>
+
+
+<?php
+  echo SSFWebPageParts::endContentHeader();
+  echo SSFWebPageParts::endPageBody();
+  echo SSFWebPageParts::endHTML();
+?>
